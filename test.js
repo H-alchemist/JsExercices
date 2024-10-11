7e25dd98752f8bb3aa1c8aaaf897774da7a57b8f
echchabli hamza <echchablihamza1@gmail.com>
2024-10-11 16:51:14 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,82 @@
+// let userN = "hamza";
+// console.log("Bonjour ," + userN );
+
+
+// let a = 32 ; 
+// let b=51 ; 
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+
+// let nom= "echchabli ";
+// let prenom="hamza";
+
+// console.log(nom + prenom);
+
+// let age= 19 ;
+// let majeur=true ; 
+
+// if (age<18) {
+   
+//    majeur=false ;
+// } 
+
+// console.log(majeur);
+
+
+// let x = 4;
+// let y=3 ; 
+// console.log(x);
+// console.log(y);
+// x=x+y;
+// y=x-y;
+// x=x-y;
+// console.log(x);
+// console.log(y);
+
+// Ex6
+
+// let rayon = 4 ; 
+
+// let c = 2*3.14*rayon;
+// console.log(c);
+
+// Ex7
+
+let celsius = 6 ;
+
+let f = (celsius * 9/5) + 32 ; 
+
+console.log(f);
+
+// Ex8
+
+let anneeNaissance = 2000;
+let anneeActuelle=2024;
+
+console.log(anneeActuelle-anneeNaissance );
+
+// Ex9
+
+let longueur = 9;
+let largeur =3
+
+let P = 2*(largeur +longueur );
+
+console.log(P);
+
+// Ex10
+
+let note1 = 12 ;
+let note2 = 15 ; 
+let note3 = 11 ; 
+let M = (note1 + note2 + note3 ) / 3 ;
+console.log(M);
+
+
+
+
+
